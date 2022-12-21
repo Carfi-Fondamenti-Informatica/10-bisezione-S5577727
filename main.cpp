772e6e1c2cc924465c533bbfc3767cbc0319ed7c
S5577727 <115453720+S5577727@users.noreply.github.com>
2022-12-21 13:56:43 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,41 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
+float c=1;
+
+float funzione (float variabile){
+    c = (variabile*variabile*cos(variabile) + 1);
+    return c;
+}
 
 int main() {
+    float a,b;
+    float err;
+    float x;
+
+    while (c>=0){
+       cout << "inserire estremi" << endl;
+       cin >> a >> b;
+       c = funzione(a) * funzione(b);
+   }
+
+    while (err >= 1e-6){
+        a=(a+b)/2;
+        x=a;
+        if((funzione(x))==0){
+            return x;
+        }
+        else {
+            if((funzione(a) * funzione(b))<0){
+                b=x;
+            }
+            else {
+                a=x;
+            }
+        }
+        err = abs(((b-a)/2));
+    }
+    cout << x;
+
    return 0;
 }
