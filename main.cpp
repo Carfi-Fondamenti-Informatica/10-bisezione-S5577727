18dcde03dd57588df3a7418cf06e9a9448b00d0b
S5577727 <115453720+S5577727@users.noreply.github.com>
2022-12-21 17:48:54 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,22 +3,24 @@
 using namespace std;
 
 float f(float var){
-    return (var*var*cos(var) + 1);
+    return (var*var*cos(var)+1);
 }
 
 int main() {
-    float a,b,x, err;
-    cout << "inserire estremi" << endl;
+    float a,b,x,err;
+    cout << "inserire estremi";
     cin >> a >> b;
+
     while ((f(a) * f(b))>=0){
-        cout << "inserire estremi" << endl;
+        cout << "inserire estremi";
         cin >> a >> b;
     }
+
     do {
         a=(a+b)/2;
         x=a;
-        float s = f(x);
-        if (s==0){
+
+        if ((f(x))==0){
             cout << x;
             break;
         }
@@ -26,12 +28,11 @@ int main() {
             if ((f(a) * f(b))<0){
                 b=x;
             }
-            else {
-                a=x;
-            }
+            else a=x;
             err = abs((b-a)/2);
         }
     }while(err >= 1e-6);
+
     cout << x;
     return 0;
 }
