7138a7ffbf7e5ae32ef6990b2893c69925c3982b
S5577727 <115453720+S5577727@users.noreply.github.com>
2022-12-21 19:35:03 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ float f(float var){
 }
 
 int main() {
-    float a=0,b=0,x,err;
+    float a=0,b=0,x=0,err;
 
     while ((f(a) * f(b))>=0){
         cout << "inserire estremi";
@@ -16,19 +16,19 @@ int main() {
 
     do {
         x=(a+b)/2;
-
         if ((f(x))==0){
             cout << x << endl;
-            cout << f(x) << endl;
             return 0;
         }
-        if ((f(x) * f(a))<0){
-                b=x;
+        else {
+            if ((f(x) * f(a)) < 0) {
+                b = x;
+            } else {
+                a = x;
+            }
+
+            err = abs((b - a) / 2);
         }
-        else a=x;
-
-        err = abs((b-a)/2);
-
     }while(err >= 1e-6);
 
     cout << int(x*10000)/10000.0;
